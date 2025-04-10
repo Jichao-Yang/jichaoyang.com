26c71c24f8da69dcec6f722efdf4052b966f6114
Jichao Yang <jichaoyang0103@gmail.com>
2025-04-09 21:39:23 -0500

    wow

--- pet.js
+++ pet.js
@@ -1,8 +1,9 @@
 const meowSound = new Audio('meow.wav');
+const wowSound = new Audio('wow.mp3');
 
 function petClick() {
     const pet = document.getElementById("pet");
-    pet.style.transform = "scale(1.3)";
+    pet.style.transform = "scale(1.5)";
 
     meowSound.currentTime = 0;  // rewind if already playing
     meowSound.play();
@@ -11,4 +12,17 @@ function petClick() {
     setTimeout(() => {
         pet.style.transform = "scale(1)";
     }, 200);
+}
+
+function ottoClick() {
+    const otto = document.getElementById("otto");
+    otto.style.transform = "scale(20)";
+
+    wowSound.currentTime = 0;  // rewind if already playing
+    wowSound.play();
+
+    
+    setTimeout(() => {
+        otto.style.transform = "scale(1)";
+    }, 1000);
 }
